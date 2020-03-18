86cddfcb4d8521e02acbb6e9685272d94a42ff55
bon_adachi <adachi_yuta@boninc.co.jp>
2020-03-18 13:24:28 +0900

    圧縮画質を変更する。

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ const webp = require('gulp-webp');
  
 gulp.task('default', () =>
     gulp.src('dev_img/**/*.{png,jpg,gif}')
-        .pipe(webp({quality: 100, method: 6}))
+        .pipe(webp({quality: 90, method: 6}))
         .pipe(gulp.dest('img/'))
 );
